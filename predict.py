2c9fbd210f94a5822f22740c2b3aadb0d99e18fc
Kaushik <koushik.varma_ug18@ashoka.edu.in>
2018-07-24 16:52:08 +0530

    Delete predict.py

--- predict.py
+++
@@ -1,8 +0,0 @@
-# Arda Mavi
-import numpy as np
-from scipy.misc import imresize
-
-def predict(model, X):
-    X = imresize(X, (150, 150, 3)).astype('float32')/255.
-    Y = model.predict(X.reshape(1,150,150,3))
-    return Y
